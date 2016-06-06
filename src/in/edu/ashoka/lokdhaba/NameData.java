a5a93ed42f75189909d5172867cb77ac407cbd29
Sudesh Prakash Singh <sudesh.singh@ashoka.edu.in>
2016-06-06 13:19:11 +0530

    add interfaces for jsp interactions

---
+++ src/in/edu/ashoka/lokdhaba/NameData.java
@@ -0,0 +1,10 @@
+package in.edu.ashoka.lokdhaba;
+
+/**
+ * Created by sudx on 6/6/16.
+ */
+public interface NameData {
+    NamePair getNamePair();
+    void setSame(NamePair np);
+    void purgeNamePair(NamePair np);
+}
---
+++ src/in/edu/ashoka/lokdhaba/NamePair.java
@@ -0,0 +1,9 @@
+package in.edu.ashoka.lokdhaba;
+
+/**
+ * Created by sudx on 6/6/16.
+ */
+public interface NamePair {
+    Row getName1();
+    Row getName2();
+}

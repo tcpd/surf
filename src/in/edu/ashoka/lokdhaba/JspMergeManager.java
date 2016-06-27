5a6d3c710eb19f36d419ad4b027ca5d02c4c8256
Sudesh Prakash Singh <sudesh.singh@ashoka.edu.in>
2016-06-27 14:04:51 +0530

    added interface for managing merges.

---
+++ src/in/edu/ashoka/lokdhaba/JspMergeManager.java
@@ -0,0 +1,122 @@
+package in.edu.ashoka.lokdhaba;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import com.google.common.collect.LinkedHashMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.MutableClassToInstanceMap;
+
+public class JspMergeManager implements MergeManager{
+	
+	Dataset d;
+	HashMap<Row, String> rowToId;
+    HashMap<String, Row> idToRow;
+    
+    ArrayList<Collection<Row>> listOfSimilarCandidates; 
+	
+	public JspMergeManager(Dataset d) {
+		this.d=d;
+	}
+
+	@Override
+	public void initializeIds() {
+		SurfExcel.assignUnassignedIds(d.getRows(), "ID");
+		
+	}
+
+	@Override
+	public void addSimilarCandidates() {
+		listOfSimilarCandidates = new ArrayList<>();
+		Multimap<String, Row> resultMap;
+		try {
+			resultMap = Bihar.getExactSamePairs(d.getRows(), d);
+			for(String canonicalVal: resultMap.keySet()){
+				listOfSimilarCandidates.add(resultMap.get(canonicalVal));
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+
+	@Override
+	public void merge(String[] ids) {
+		String defaultId = ids[0];
+    	for(int i=1;i<ids.length;i++) {
+    		Row tempRow = idToRow.get(ids[i]);
+    		rowToId.put(tempRow, defaultId);
+    	}
+		
+	}
+
+	@Override
+	public void deMerge(String[] ids) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void performInitialMapping() {
+		rowToId = new HashMap<>();
+		idToRow = new HashMap<>();
+		for(Row row:d.getRows()){
+    		rowToId.put(row, row.get("ID"));
+    		idToRow.put(row.get("ID"), row);
+    	}
+	}
+	
+	public void display(){
+		int i=0;
+		for(Collection<Row> similarRows:listOfSimilarCandidates){
+			System.out.println("Collection "+i+":==========================================================");
+			i++;
+			for(Row row:similarRows){
+				System.out.println(row);
+			}
+		}
+	}
+	
+	public void display2(){
+		ArrayList<Multimap<String, Row>> similarIncumbents = getIncumbents();
+		int i=0;
+		for(Multimap<String,Row> mp:similarIncumbents) {
+			System.out.println("Group "+i+"================================================");
+			for(String key:mp.keySet()){
+				System.out.println("Similar people:"+key+"----------------------------------------");
+				for(Row row:mp.get(key)){
+					System.out.println(row);
+				}
+			}
+		}
+	}
+
+	@Override
+	public ArrayList<Multimap<String, Row>> getIncumbents() {
+		ArrayList<Multimap<String, Row>> listOfSet = new ArrayList<>();
+		for(Collection<Row> similarRows:listOfSimilarCandidates){
+			Multimap<String, Row> mp = LinkedHashMultimap.create();
+			for(Row row:similarRows){
+				mp.put(rowToId.get(row), row);
+			}
+			listOfSet.add(mp);
+		}
+		return listOfSet;
+	}
+	
+	
+	
+}
---
+++ src/in/edu/ashoka/lokdhaba/MergeManager.java
@@ -0,0 +1,24 @@
+package in.edu.ashoka.lokdhaba;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+import com.google.common.collect.Multimap;
+
+public interface MergeManager {
+	
+	//initialize the id's for each row
+	public void initializeIds();
+	
+	void performInitialMapping();
+	
+	//add candiates which will be judged based on their group
+	public void addSimilarCandidates();
+	
+	public void merge(String [] ids);
+	public void deMerge(String [] ids);
+	public void save();
+	public ArrayList<Multimap<String,Row>> getIncumbents();
+
+}

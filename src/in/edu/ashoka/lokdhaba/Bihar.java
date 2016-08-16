dfdee88841c5aefaa04576a72f7d71c1b6a1ac96
Sudesh Prakash Singh <sudesh.singh@ashoka.edu.in>
2016-08-16 17:00:53 +0530

    previous bugfix didn't work. Fixed now.

--- src/in/edu/ashoka/lokdhaba/Bihar.java
+++ src/in/edu/ashoka/lokdhaba/Bihar.java
@@ -311,7 +311,7 @@ public class Bihar extends Object {
 	        
 	        //remove nota
 	        resultMap.asMap().remove("ab he ne no of ove");
-	        resultMap.asMap().remove("AB HE NE NO OF OVE");
+	        resultMap.asMap().remove("AB NONE OF OWE TE");
 	        
 	        //remove groups where position is below 5==== EXPERIMENTAL
 	        

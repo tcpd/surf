729b9c2f3f016ab5230d2421b15892b5d4e7df38
Sudesh Prakash Singh <sudesh.singh@ashoka.edu.in>
2017-04-13 14:56:32 +0530

    changed search query mechanism to use regex instead of fuzzy search

--- src/in/edu/ashoka/lokdhaba/SearchMergeManager.java
+++ src/in/edu/ashoka/lokdhaba/SearchMergeManager.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by sudx on 6/4/17.
@@ -31,31 +33,20 @@ public class SearchMergeManager extends MergeManager {
         if(searchQuery==null||searchQuery.equals(""))
         {}
         else{
-            int thresholdDistance = 3;
-            int weight =10;
             Multimap<Integer,Row> matchDistance = LinkedHashMultimap.create();
-            String searchQueryFlattened = searchQuery.replace(" ","").toLowerCase();
+            Pattern pattern = Pattern.compile(searchQuery, Pattern.CASE_INSENSITIVE);
+            String falttenedSearchQuery = searchQuery.replace("[^\\p{L}\\p{Z}]","");
             for(Row row:d.getRows()){
-                String combined = "";
-                combined += row.get("Name")+" "+row.get("Year")+" "+row.get("PC_name")+ " " + row.get("State");
-                String nameFlattened = row.get("Name").replace(" ","").toLowerCase();
-                if(nameFlattened.contains(searchQueryFlattened)){
-                    matchDistance.put(StringUtils.getLevenshteinDistance(searchQueryFlattened,nameFlattened),row);
-                    continue;
-                }
-                int distance =  StringUtils.getLevenshteinDistance(searchQueryFlattened,nameFlattened);
-                if(distance < thresholdDistance){
-                    matchDistance.put(distance*weight,row);
+                Matcher matcher = pattern.matcher(StringUtils.normalizeSpace(row.get("Name")));
+                if(matcher.find()||matcher.matches()){
+                    //group.add(row);
+                    matchDistance.put(StringUtils.getLevenshteinDistance(falttenedSearchQuery,row.get("Name")),row);
                 }
             }
+
             Collection<Integer> distanceKeys = matchDistance.keySet();
             List<Integer> distanceList = new ArrayList<>(distanceKeys);
-            distanceList.sort(new Comparator<Integer>() {
-                @Override
-                public int compare(Integer o1, Integer o2) {
-                    return o1-o2;
-                }
-            });
+            distanceList.sort((o1,o2)->(o1-o2));
             Collection<Row> group = new ArrayList<>();
             for(int currentDistance:distanceList){
                 group.addAll(matchDistance.get(currentDistance));

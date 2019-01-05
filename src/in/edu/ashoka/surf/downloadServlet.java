694c91d534a9ca18618a19fc4711a13d3accfe63
checker5965 <archit.checker_ug20@ashoka.edu.in>
2019-01-05 21:40:18 +0530

    added backend for downloading

---
+++ src/in/edu/ashoka/surf/downloadServlet.java
@@ -0,0 +1,57 @@
+package in.edu.ashoka.surf;
+
+import java.io.IOException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.annotation.*;
+
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+@MultipartConfig
+public class downloadServlet extends HttpServlet {
+    public static Log log = LogFactory.getLog(in.edu.ashoka.surf.downloadServlet.class);
+
+    public downloadServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/plain");
+        log.warn("Inside Servlet");
+        HttpSession session = request.getSession();
+        String key = (String) session.getAttribute("datasetKey");
+        response.setHeader("Content-disposition", "attachment; filename="+key+"_surf.csv");
+        if(!(new File(System.getProperty("user.home")+File.separator+"Surf Data"+File.separator+key+".csv").exists()))
+            request.getRequestDispatcher("table.jsp").include(request, response);
+        else
+        {
+            //https://www.baeldung.com/servlet-download-file
+            FileInputStream file = new FileInputStream(System.getProperty("user.home")+File.separator+"Surf Data"+File.separator+key+".csv");
+            OutputStream out = response.getOutputStream();
+            byte[] buffer = new byte[512];
+            int numBytesRead;
+            while ((numBytesRead = file.read(buffer)) > 0) 
+                out.write(buffer, 0, numBytesRead);
+            file.close();
+            log.warn("read file");
+            out.close();
+        }
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+	}
+
+	public void destroy() {
+	}
+}

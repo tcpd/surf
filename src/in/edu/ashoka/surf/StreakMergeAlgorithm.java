6b1306b8ce0f63e66dd7feb968171f6fecec69c9
hangal <hangal@stanfordalumni.org>
2019-03-07 14:52:04 +0530

    minor code cleanup in streaks alg

--- src/in/edu/ashoka/surf/StreakMergeAlgorithm.java
+++ src/in/edu/ashoka/surf/StreakMergeAlgorithm.java
@@ -5,8 +5,9 @@ import in.edu.ashoka.surf.util.Pair;
 import in.edu.ashoka.surf.util.Util;
 
 import java.util.*;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toSet;
+import static java.util.stream.Collectors.toList;
 
 /**
  * Created by hangal on 8/12/17.
@@ -24,14 +25,6 @@ public class StreakMergeAlgorithm extends MergeAlgorithm {
     private int maxHoles;
     private String secondaryFieldName;
     private Filter filter;
-
-    // small class to get around the problem of non-final vars not being available inside the lambda
-    class InternalPredicate implements Predicate<Row> {
-        int v;
-        InternalPredicate(int val) { this.v = val;}
-        @Override
-        public boolean test(Row r) { return Integer.parseInt(r.get(streakFieldName)) == v; }
-    }
 
     /* set up merge algorithm parameters: d, the fieldName (col. name) of the field on which edit distance clustering is to be done, max. editDistance (inclusive) */
     StreakMergeAlgorithm(Dataset dataset, Filter filter, String streakFieldName, int streakLength, int maxHoles, String secondaryFieldName) {
@@ -109,19 +102,20 @@ public class StreakMergeAlgorithm extends MergeAlgorithm {
 
     @Override
     public List<Collection<Row>> run() {
-        Collection<Row> filteredRows = dataset.getRows().stream().filter(filter::passes).collect(Collectors.toList());
+        Collection<Row> filteredRows = dataset.getRows().stream().filter(filter::passes).collect(toList());
 
         // compute maps we'll need later
         Multimap<String, Row> idToRows = SurfExcel.split (filteredRows, Config.ID_FIELD);
         Multimap<String, Row> streakFieldToRows = SurfExcel.split (filteredRows, streakFieldName);
 
+        Set<String> uniqueIDs = new LinkedHashSet<>(idToRows.keys());
         // go over all ids one by one. use a set on idToRows.keys() because otherwise it returns same key multiple times
         outer:
-        for (String id: new LinkedHashSet<>(idToRows.keys())) {
+        for (String id: uniqueIDs) {
             final Collection<Row> rowsForThisID = idToRows.get(id);
 
             // get all unique vals in the streak field, converted to int
-            Set<Integer> uniqueValsInStreakField = rowsForThisID.stream().map(r -> Integer.parseInt(r.get(streakFieldName))).collect(Collectors.toSet());
+            Set<Integer> uniqueValsInStreakField = rowsForThisID.stream().map(r -> Integer.parseInt(r.get(streakFieldName))).collect(toSet());
 
             // no need to look at "streaks" of length just 1
             if (uniqueValsInStreakField.size() == 1)
@@ -136,48 +130,48 @@ public class StreakMergeAlgorithm extends MergeAlgorithm {
             // TOFIX: problem here. Assume Streak length is 5, and maxHoles is 1.
             // if uniqueValsInStreakField is 1, 4, 5, 7, 8, we will think there are 3 holes (2, 3, 6) and therefore bail out
             // but if you look at the run from 4..8, that matches streak of length 5 with only one hole
-            Set<Integer> holeValues = new LinkedHashSet<>(); // this will have the holes in the streak field
+            Set<Integer> holes = new LinkedHashSet<>(); // this will have the holes in the streak field
             for (int val = minValInStreak; val <= maxValInStreak; val++) {
                 if (!uniqueValsInStreakField.contains(val)) {
                     // this val is a hole in the streak
-                    holeValues.add(val);
+                    holes.add(val);
 
-                    if (holeValues.size() > maxHoles)
+                    if (holes.size() > maxHoles)
                         continue outer; // this id has more than maxHoles, so no need to conside it
                 }
             }
-            if (holeValues.size() == 0)
+            if (holes.size() == 0)
                 continue; // if there are no holes, nothing to look for
 
-            Set<String> secondaryValsForThisId = rowsForThisID.stream().map(r -> r.get(secondaryFieldName)).collect(Collectors.toSet());
+            Set<String> secondaryValsForThisId = rowsForThisID.stream().map(r -> r.get(secondaryFieldName)).collect(toSet());
 
             log.info("Looking for streak candidates for id: " + id + " with " + rowsForThisID.size() + " rows, "
-                    + holeValues.size() + " hole(s): " + Util.join (holeValues, ",")
+                    + holes.size() + " hole(s): " + Util.join (holes, ",")
                     + ", " + secondaryValsForThisId.size() + " secondary value(s): " + Util.join(secondaryValsForThisId, ","));
-            log.info (Util.join (rowsForThisID.stream().map(r -> r.get(Config.MERGE_FIELD)).collect(Collectors.toSet()), ","));
+            log.info (Util.join (rowsForThisID.stream().map(r -> r.get(Config.MERGE_FIELD)).collect(toSet()), ","));
 
             // mark the streak rows for this PID as special, so it will be shown as .special-row in the UI
-            rowsForThisID.forEach(r -> { r.set("__is_special", "true"); });
+            rowsForThisID.forEach(r -> r.set("__is_special", "true"));
 
             // select all rows with holeValues, and secondaryField in one of the secondaryValsForThisId
             // for every such row, also compute average distance from streakRows
             List<Pair<Row, Float>> holeCandidateAndDistance = new ArrayList<>();
-            for (int holeValue: holeValues) {
+            for (int holeValue: holes) {
                 Collection<Row> rowsInHole = streakFieldToRows.get(Integer.toString(holeValue));
-                Collection<Row> holeCandidates = rowsInHole.stream().filter(r -> secondaryValsForThisId.contains(r.get(secondaryFieldName))).collect(Collectors.toSet());
+                Collection<Row> holeCandidates = rowsInHole.stream().filter(r -> secondaryValsForThisId.contains(r.get(secondaryFieldName))).collect(toSet());
 
                 // for every hole candidate row, create a pair with that row and it's distance from streak rows
-                holeCandidateAndDistance.addAll (holeCandidates.stream().map(r -> rowToStreakRowsDistance(r, rowsForThisID)).collect(Collectors.toList()));
+                holeCandidateAndDistance.addAll (holeCandidates.stream().map(r -> rowToStreakRowsDistance(r, rowsForThisID)).collect(toList()));
 
                 log.info(holeCandidates.size()  + "rows as candidates for hole: " + holeValue + " in id: " + id);
             }
 
             // sort holecandidates by decreasing distance
             Util.sortPairsBySecondElement(holeCandidateAndDistance);
-            // but we actually want decreasing distance
+            // but we actually want increasing distance
             Collections.reverse(holeCandidateAndDistance);
             // now extract only the rows
-            List<Row> holeCandidateRows = holeCandidateAndDistance.stream().map (Pair::getFirst).collect(Collectors.toList());
+            List<Row> holeCandidateRows = holeCandidateAndDistance.stream().map (Pair::getFirst).collect(toList());
 
             // create a cluster; streak rows first, and then the hole candidate rows
             Collection<Row> thisCluster = new ArrayList<>();
